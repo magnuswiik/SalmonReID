51f83a0bae87708cc00f470b03a6797e280c99be
Magnus <magnuswii@gmail.com>
2024-05-03 12:33:39 +0200

    endere epochs

--- IDUNfiles/feature_extraction/identification.py
+++ IDUNfiles/feature_extraction/identification.py
@@ -237,7 +237,7 @@ def train_model(model, train_loader, validation_loader, hyperparameters, device,
 
 def main():
     
-    EPOCHS = 500 if torch.cuda.is_available() else 10
+    EPOCHS = 100 if torch.cuda.is_available() else 50
     BATCHSIZE = 5 if torch.cuda.is_available() else 2
     
     hyperparameters_AP1 = {
